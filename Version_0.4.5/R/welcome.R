e0610b597a989a50264d14ec6eee0ee00d3351f7
liubingdong <liubingdong221@gmail.com>
2021-07-08 16:25:59 +0800

    add welcome

---
+++ Version_0.4.5/R/welcome.R
@@ -0,0 +1,4 @@
+.onAttach <- function(libname, pkgname) {
+  packageStartupMessage("EMP is ready to use.\n")
+  packageStartupMessage("Suggestions and bug-reports can be submitted at: https://github.com/liubingdong/EasyMicroPlot")
+}
